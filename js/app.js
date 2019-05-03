a28881f7350c1c52d5492744c63198db10cecc12
Gian Masto <gianmasto1@gmail.com>
2019-05-02 22:11:36 -0300

    Servicios section finished. Galeria section added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+  // HAMBURGER MENU
+
 let navbarList = document.getElementById('js-list');
 let navbarBurger = document.getElementById('js-burger');
 
@@ -8,3 +10,57 @@ let dropdown = () => {
 }
 
 dropdown();
+
+  // SLIDER
+
+let images = document.querySelectorAll('.slide'),
+    arrowLeft = document.getElementById('arrow-left'),
+    arrowRight = document.getElementById('arrow-right'),
+    current = 0;
+
+console.log(images);
+
+// Clear all images
+function reset() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.display = 'none';
+  }
+}
+
+// Start slider
+function startSlide() {
+  reset();
+  images[0].style.display = 'block';
+}
+
+//Show previous image
+function slideLeft() {
+  reset();
+  images[current - 1].style.display = 'block';
+  current--;
+}
+
+//Show next image
+function slideRight() {
+  reset();
+  images[current + 1].style.display = 'block';
+  current++;
+}
+
+//Left arrow click
+arrowLeft.addEventListener('click', () => {
+  if (current === 0) {
+    current = images.length;
+  }
+  slideLeft();
+});
+
+//Right arrow click
+arrowRight.addEventListener('click', () => {
+  if (current === images.length - 1) {
+    current = -1;
+  }
+  slideRight();
+});
+
+startSlide();
